5e54b4977c1608fd618afe2dd57ea70151b92947
xavier eli arias perez <xarias13@gmail.com>
2023-10-07 14:43:49 -0300

    feat(app): add mongodb first iteration

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,17 @@ use actix_web::{App, web, HttpServer};
 use envy;
 use dotenvy;
 use config::Configuration;
+use mongodb::{Database, options::ClientOptions};
+
+async fn connect_database(address: &str, port: &str, user: &str, password: &str, database: &str) -> Database {
+    let client_options = ClientOptions::parse(format!("mongodb://{user}:{password}@{address}:{port}/{database}"))
+        .await
+        .unwrap();
+
+    let client = mongodb::Client::with_options(client_options).unwrap();
+
+    client.database(database)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -18,13 +29,21 @@ async fn main() -> std::io::Result<()> {
     }
     
     let config = envy::from_env::<Configuration>()
-    .expect("Please provide env vars");
+        .expect("Please provide env vars");
+
+    let conn = connect_database(
+        "0.0.0.0",
+        "27017",
+        "root",
+        "root",
+        "core"
+    ).await;
 
     HttpServer::new(|| {
         App::new()
         .service(controllers::app::home)
         .service(
-    web::scope("/api")
+            web::scope("/api")
                 .service(controllers::app::home)
                 .service(
                     web::scope("/users").configure(controllers::user::scope)         
